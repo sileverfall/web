a82fffd8bed1ec595d70c9053977464cf7257cf6
club1 <club1@localhost.localdomain>
2017-09-26 12:11:28 +0800

    revise

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var settings = require('./settings');
@@ -26,6 +28,7 @@ var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(flash());
@@ -35,10 +38,12 @@ app.use(flash());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+routes(app);
 
 app.use(session({
     secret: settings.cookieSecret,
