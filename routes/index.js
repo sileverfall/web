4dcb82b47406e7d1bc2d8e1bcbd2189af85c7a61
club1 <club1@localhost.localdomain>
2017-09-26 12:04:09 +0800

    revise

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+//
+//module.exports = router;
 
-module.exports = router;
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', {
+            title: 'Express'
+        });
+    });
+    app.get('/contents', function (req, res) {
+        res.render('contents', {
+            title: '內容'
+        });
+    });
+};
